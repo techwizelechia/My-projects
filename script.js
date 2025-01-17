f4b8ad50bb552f668c1ecc635589afd0775adcdf
Elechia Juma <otienoelechia3@gmail.com>
2025-01-17 15:05:31 +0300

    Add folder with files

---
+++ script.js
@@ -0,0 +1,25 @@
+// // Creating an Array
+// const fruits = ["mangoes", "Oranges", "Pinnaples"];
+// console.log(fruits);
+
+// // Adding an items to an Array
+// fruits.push("Peas");
+// console.log(fruits);
+
+// // Adding items at the start of an Array
+// fruits.unshift("Guava");
+// console.log(fruits);
+
+// // Removing the last item in an Array
+// fruits.pop();
+// console.log(fruits);
+
+// // Removing the firts item in Array
+// fruits.shift();
+// console.log(fruits);
+
+// // Removing an item from an Array using Index
+// fruits.splice(1, 1);
+// console.log(fruits);
+
+// //
